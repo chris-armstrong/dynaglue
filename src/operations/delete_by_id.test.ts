19c79ff7cb8a1d505aad5e8888827bdf6c9c6550
Chris Armstrong <chris.armstrong@gorillastack.com>
2019-11-28 21:13:09 +1100

    add deleteById tests

---
+++ src/operations/delete_by_id.test.ts
@@ -0,0 +1,69 @@
+import { CollectionNotFoundException } from "../exceptions";
+import { deleteById } from "./delete_by_id";
+import { createContext } from "../context";
+import { DynamoDB } from "aws-sdk/clients/all";
+import { wrap } from "module";
+import { toWrapped } from "../util";
+import { Converter } from "aws-sdk/clients/dynamodb";
+
+describe('deleteById', () => {
+  const layout = {
+    tableName: 'testtable',
+    primaryKey: { partitionKey: 'id', sortKey: 'collection' },
+  };
+
+  const collection = {
+    name: 'test-collection',
+    layout,
+  };
+
+  test('throws when the collection does not exist', () => {
+    const context = createContext({} as DynamoDB, [collection]);
+    expect(deleteById(context, 'not-a-collection', 'idvalue')).rejects.toThrowError(CollectionNotFoundException);
+  });
+
+  test('returns undefined when there is no old value', async () => {
+    const mock = {
+      deleteItem: jest.fn().mockReturnValue({ promise: jest.fn().mockResolvedValue({}) }),
+    };
+    const context = createContext(mock as unknown as DynamoDB, [collection]);
+    const result = await deleteById(context, 'test-collection', 'idvalue');
+
+    expect(mock.deleteItem.mock.calls[0][0]).toEqual({
+      TableName: 'testtable',
+      Key: {
+        id: { S: 'idvalue' },
+        collection: { S: 'test-collection' },
+      },
+      ReturnValues: 'ALL_OLD',
+    });
+    expect(result).toBeUndefined();
+  });
+
+  test('returns old value when it is returned', async () => {
+    const value = {
+      _id: 'idvalue',
+      name: 'Test Name',
+    };
+
+    const mock = {
+      deleteItem: jest.fn().mockReturnValue({
+        promise: jest.fn().mockResolvedValue({
+          Attributes: Converter.marshall({ value }),
+        }),
+      }),
+    };
+    const context = createContext(mock as unknown as DynamoDB, [collection]);
+    const result = await deleteById(context, 'test-collection', 'idvalue');
+
+    expect(mock.deleteItem.mock.calls[0][0]).toEqual({
+      TableName: 'testtable',
+      Key: {
+        id: { S: 'idvalue' },
+        collection: { S: 'test-collection' },
+      },
+      ReturnValues: 'ALL_OLD',
+    });
+    expect(result).toEqual(value);
+  })
+});
