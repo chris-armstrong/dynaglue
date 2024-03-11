35f4a1b0cdbab8eeca04ed2225afd49d8bf1c3d2
nik <nik+github@honeyinsurance.com>
2024-03-11 12:26:10 +1100

    changes to write changes to DDB using transaction

--- src/base/exceptions.ts
+++ src/base/exceptions.ts
@@ -12,6 +12,16 @@ export class ConflictException extends VError {
 }
 
 /**
+ * Thrown when a replace or delete request(s) are rejected as part of transaction
+ * that already exists
+ */
+export class TransactionConflictException extends VError {
+  constructor(message: string) {
+    super({ name: 'transaction_conflict.error' }, message);
+  }
+}
+
+/**
  * Thrown when an `_id` value is specified that is not
  * valid (_id values must be a string).
  */
@@ -118,7 +128,7 @@ export class InvalidIndexedFieldValueException extends VError {
 export class InvalidQueryException extends VError {
   constructor(
     message: string,
-    { collection, query }: { collection: string; query: Record<string, unknown> }
+        { collection, query }: { collection: string; query: Record<string, unknown> }
   ) {
     super(
       {
@@ -232,6 +242,17 @@ export class InvalidBatchReplaceDeleteDescriptorException extends VError {
   }
 }
 
+/**
+ * When a TransactGetItems request conflicts with an
+ * ongoing TransactWriteItems operation on one or
+ * more items in the TransactGetItems request
+ */
+export class TransactionCanceledException extends VError {
+  constructor(message: string, info?: Record<string, unknown>) {
+    super({ name: 'transaction_cancelled', info }, message);
+  }
+}
+
 export class InvalidRangeOperatorException extends VError {
   constructor(message: string, operator: string) {
     super({ name: 'invalid_range_operator', info: { operator } }, message);
@@ -241,5 +262,23 @@ export class InvalidRangeOperatorException extends VError {
 export class IndexAccessPatternTypeException extends VError {
   constructor(message: string) {
     super({ name: 'index_access_pattern_type'}, message);
+  }
+}
+
+/**
+ * DynamoDB rejected the request because you retried a request with
+ * a different payload but with an idempotent token that was already used.
+ */
+export class IdempotentParameterMismatchException extends VError {
+  constructor(message: string) {
+    super({ name: 'idempotent_parameter_mismatch' }, message);
+  }
+}
+/**
+ * The transaction with the given request token is already in progress
+ */
+export class TransactionInProgressException extends VError {
+  constructor(message: string) {
+    super({ name: 'transaction_in_progress' }, message);
   }
 }
--- src/operations/replace.ts
+++ src/operations/replace.ts
@@ -2,7 +2,7 @@ import { PutItemCommand, PutItemInput } from '@aws-sdk/client-dynamodb';
 import { marshall } from '@aws-sdk/util-dynamodb';
 import { Context } from '../context';
 import { toWrapped, getCollection } from '../base/util';
-import { DocumentWithId } from '../base/common';
+import { DocumentWithId, WrappedDocument } from '../base/common';
 import { createNameMapper, createValueMapper } from '../base/mappers';
 import debugDynamo from '../debug/debugDynamo';
 import { CompositeCondition } from '../base/conditions';
@@ -20,7 +20,7 @@ import { parseCompositeCondition } from '../base/conditions_parser';
  * @param collectionName the collection to update
  * @param value the document to insert or replace
  * @param options options to apply
- * @param options.condition an optional conditional expression that must be satifisfied for the update to proceed
+ * @param options.condition an optional conditional expression that must be satisfied for the update to proceed
  * @returns the inserted / replaced value
  * @throws {CollectionNotFoundException} when the collection is not found
  */
@@ -30,6 +30,41 @@ export async function replace<DocumentType extends DocumentWithId>(
   value: Record<string, unknown>,
   options: { condition?: CompositeCondition } = {}
 ): Promise<DocumentType> {
+  const { request, wrapped } = createReplaceByIdRequest(
+    context,
+    collectionName,
+    value,
+    options
+  );
+
+  debugDynamo('PutItem', request);
+  const command = new PutItemCommand(request);
+  await context.ddb.send(command);
+  return (wrapped as WrappedDocument<DocumentType>).value;
+}
+
+/**
+ * Create a request for Insert or Replace request
+ *
+ * This operation differs from [[insert]] in that it does not check for the existence
+ * of a document with the same `_id` value - it will replace whatever is there.
+ *
+ * @category Mutation
+ *
+ * @param context the context
+ * @param collectionName the collection to update
+ * @param value the document to insert or replace
+ * @param options options to apply
+ * @param options.condition an optional conditional expression that must be satisfied for the update to proceed
+ * @returns the inserted / replaced request @see {PutItemInput}
+ * @throws {CollectionNotFoundException} when the collection is not found
+ */
+export const createReplaceByIdRequest = <DocumentType extends DocumentWithId>(
+  context: Context,
+  collectionName: string,
+  value: Record<string, unknown>,
+  options: { condition?: CompositeCondition } = {}
+): { request: PutItemInput; wrapped: WrappedDocument<DocumentType> } => {
   const collection = getCollection(context, collectionName);
   const wrapped = toWrapped<DocumentType>(collection, value);
 
@@ -44,16 +79,18 @@ export async function replace<DocumentType extends DocumentWithId>(
     });
   }
 
+  const item = marshall(wrapped, {
+    convertEmptyValues: false,
+    removeUndefinedValues: true,
+  });
   const request: PutItemInput = {
     TableName: collection.layout.tableName,
-    Item: marshall(wrapped, { convertEmptyValues: false, removeUndefinedValues: true }),
+    Item: item,
     ReturnValues: 'NONE',
     ConditionExpression: conditionExpression,
     ExpressionAttributeNames: nameMapper.get(),
     ExpressionAttributeValues: valueMapper.get(),
   };
-  debugDynamo('PutItem', request);
-  const command = new PutItemCommand(request);
-  await context.ddb.send(command);
-  return wrapped.value;
-}
+
+  return { request, wrapped };
+};
